a1d2f24466a11a3df5c0235a3423503ef6ff0723
Pierre-François Gimenez <pierre-francois.gimenez@centralesupelec.fr>
2020-10-26 16:16:15 +0100

    Update setup scripts

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ecf",
-    version="0.1",
+    version="0.2",
     author="Pierre-François Gimenez",
     author_email="<pierre-francois.gimenez@laas.fr>",
     description="Empirical Christoffel function for anomaly detection",
@@ -13,10 +13,15 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/PFGimenez/ecf-anomaly-detection",
     packages=setuptools.find_packages(),
+    install_requires=[
+          'numpy',
+          'scikit-learn',
+      ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+
 )
